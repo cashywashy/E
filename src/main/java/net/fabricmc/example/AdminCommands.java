4e633219a24213cf06e83803829b83c52a611bf6
cashywashy <zhao.kevin725@gmail.com>
2022-06-02 21:38:44 -0400

    I DID STUFF AND IM ANGRY ABOUT STUFF ETC.

--- src/main/java/net/fabricmc/example/AdminCommands.java
+++ src/main/java/net/fabricmc/example/AdminCommands.java
@@ -3,14 +3,19 @@ package net.fabricmc.example;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.EnderChestInventory;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.AirBlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public class AdminCommands {
-    public static int peepEnderChest(CommandContext context){
+    public static int peepEnderChest(CommandContext context) {
         try {
             ServerPlayerEntity player2 = EntityArgumentType.getPlayer(context, "player");
             EnderChestInventory enderChestInventory = player2.getEnderChestInventory();
@@ -25,13 +30,106 @@ public class AdminCommands {
 
         return 0;
     }
-    public static int peepInventory(CommandContext context){
+
+    public static int peepInventory(CommandContext context) {
         try {
             ServerPlayerEntity player2 = EntityArgumentType.getPlayer(context, "player");
             PlayerInventory playerInventory = player2.getInventory();
             playerInventory.onOpen(player2);
-//            player2.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId, inventory, player) -> GenericContainerScreenHandler.createGeneric9x6(syncId, inventory, playerInventory), player2.getName()));
-//            player2.openHandledScreen(SimpleNamedScreenHandlerFactory)
+
+            SimpleNamedScreenHandlerFactory menu = new SimpleNamedScreenHandlerFactory((syncId, inventory, player) -> {
+                Inventory e = new Inventory() {
+                    @Override
+                    public int size() {
+                        return 54;
+                    }
+
+                    @Override
+                    public boolean isEmpty() {
+                        return playerInventory.isEmpty();
+                    }
+
+                    /**
+                     * Adjusts the view slot index to the {@link PlayerInventory.combinedInventory} slot index.
+                     *
+                     * @param slot the view slot index.
+                     * @return the {@link PlayerInventory.combinedInventory} slot index, or -1 if the view slot does not have a corresponding real slot.
+                     */
+                    public int getSlot(int slot) {
+                        if (slot < playerInventory.main.size()) {
+                            return slot;
+                        }
+
+                        // remove padding row.
+                        slot -= 9;
+
+                        if (slot >= playerInventory.main.size() && slot < playerInventory.main.size() + playerInventory.armor.size()) {
+                            return slot;
+                        }
+
+                        // remove offhand alignment to the right side of the container.
+                        slot -= 4;
+
+                        if (slot == playerInventory.main.size() + playerInventory.armor.size()) {
+                            return slot;
+                        }
+
+                        return -1;
+                    }
+
+                    @Override
+                    public ItemStack getStack(int slot) {
+                        slot = getSlot(slot);
+                        return slot == -1 ? ItemStack.EMPTY : playerInventory.getStack(slot);
+                    }
+
+                    @Override
+                    public ItemStack removeStack(int slot, int amount) {
+                        slot = getSlot(slot);
+                        return slot == -1 ? ItemStack.EMPTY : playerInventory.removeStack(slot, amount);
+                    }
+
+                    @Override
+                    public ItemStack removeStack(int slot) {
+                        slot = getSlot(slot);
+                        return slot == -1 ? ItemStack.EMPTY : playerInventory.removeStack(slot);
+                    }
+
+                    @Override
+                    public void setStack(int slot, ItemStack stack) {
+                        slot = getSlot(slot);
+                        if (slot != -1) {
+                            playerInventory.setStack(slot, stack);
+                        }
+                    }
+
+                    @Override
+                    public void markDirty() {
+                        playerInventory.markDirty();
+                    }
+
+                    @Override
+                    public boolean canPlayerUse(PlayerEntity player) {
+                        return true;
+                    }
+
+                    @Override
+                    public void clear() {
+                        playerInventory.clear();
+                    }
+
+                    @Override
+                    public boolean isValid(int slot, ItemStack stack) {
+                        return getSlot(slot) != -1;
+                    }
+                };
+                return GenericContainerScreenHandler.createGeneric9x6(syncId, inventory, e);
+
+            }, player2.getName());
+
+
+            player2.openHandledScreen(menu);
+
         } catch (CommandSyntaxException e) {
             // TODO: shout at caller
             e.printStackTrace();
---
+++ src/main/java/net/fabricmc/example/mixin/ScreenHandlerMixin.java
@@ -0,0 +1,45 @@
+package net.fabricmc.example.mixin;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.GenericContainerScreenHandler;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.screen.slot.SlotActionType;
+import net.minecraft.util.collection.DefaultedList;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ScreenHandler.class)
+public abstract class ScreenHandlerMixin {
+    @Shadow
+    protected abstract void internalOnSlotClick(int slotIndex, int button, SlotActionType actionType, PlayerEntity player);
+
+    @Shadow @Final public static int EMPTY_SPACE_SLOT_INDEX;
+
+    @Shadow @Final public DefaultedList<Slot> slots;
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/ScreenHandler;internalOnSlotClick(IILnet/minecraft/screen/slot/SlotActionType;Lnet/minecraft/entity/player/PlayerEntity;)V"), method = "onSlotClick")
+    public void PooPooPeePee(ScreenHandler instance, int slotIndex, int button, SlotActionType actionType, PlayerEntity player) {
+        if (slotIndex != -999 && slotIndex != -1) {
+            Slot slot = instance.slots.get(slotIndex);
+            if (instance.getType().equals(ScreenHandlerType.GENERIC_9X6)){
+                GenericContainerScreenHandler hando = (GenericContainerScreenHandler) instance;
+                if (!hando.getInventory().isValid(slotIndex, slot.getStack()) && actionType == SlotActionType.QUICK_MOVE) return;
+            }
+            if (!slot.inventory.isValid(slotIndex, slot.getStack())) return;
+        }
+        this.internalOnSlotClick(slotIndex, button, actionType, player);
+    }
+
+    @Inject(at = @At("HEAD"), method = "canInsertIntoSlot(Lnet/minecraft/item/ItemStack;Lnet/minecraft/screen/slot/Slot;)Z", cancellable = true)
+    public void PeePeePooPoo(ItemStack stack, Slot slot, CallbackInfoReturnable<Boolean> cir){
+        cir.setReturnValue(slot.inventory.isValid(slot.id, stack));
+    }
+}
