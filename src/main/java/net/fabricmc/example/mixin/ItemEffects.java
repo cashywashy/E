749351becc89190b6771df539a0f3ff1c5a46fd1
cashywashy <zhao.kevin725@gmail.com>
2022-06-07 13:17:44 -0400

    EffectItems won't be consumed when they're not supposed to

--- src/main/java/net/fabricmc/example/mixin/ItemEffects.java
+++ src/main/java/net/fabricmc/example/mixin/ItemEffects.java
@@ -2,6 +2,7 @@ package net.fabricmc.example.mixin;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
@@ -10,12 +11,16 @@ import net.minecraft.nbt.NbtInt;
 import net.minecraft.nbt.NbtString;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -33,21 +38,32 @@ public final class ItemEffects {
 	 * @param hand the {@link  Hand}... I don't know what this is for, but it's in the original method, so I had to add it here.
 	 * @param cir the {@link CallbackInfoReturnable<ActionResult>}.
 	 **/
-	@Inject(at = @At("HEAD"), method = "interactItem")
+	@Inject(at = @At("HEAD"), method = "interactItem", cancellable = true)
+	public void interactItemOverride(ServerPlayerEntity player, World world, ItemStack stack, Hand hand, CallbackInfoReturnable<ActionResult> cir){
+		EffectActivation(player,world,stack,hand,cir);
+	}
+
+	@Inject(at = @At("HEAD"), method = "interactBlock", cancellable = true)
+	public void interactBlockOverride(ServerPlayerEntity player, World world, ItemStack stack, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir){
+		EffectActivation(player,world,stack,hand,cir);
+	}
+
 	public void EffectActivation(ServerPlayerEntity player, World world, ItemStack stack, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
-		if (world.isClient()) return;
 		if (stack.hasNbt() && stack.getNbt().contains("SpecialEffect")) {
+			if (world.isClient()) cir.setReturnValue(ActionResult.PASS);
 			NbtCompound effect = stack.getNbt().getCompound("SpecialEffect");
 			if (effect.contains("id", NbtElement.STRING_TYPE) && effect.contains("power"))
 				switch (effect.getString("id")) {
 					case (EXPLOSION) -> BoomBoom(player, effect, world);
 					case (YOUSHOULDKILLYOURSELFNOW) -> YouShouldKillYourselfNOW(player, effect, world);
 				}
+			cir.setReturnValue(ActionResult.SUCCESS);
 		}
+
 	}
 
 	/**
-	 * The EXPLOSION method. Creates an explosion on the {@link PlayerEntity}, by the {@link PlayerEntity}.
+	 * Bakuretsu! Bakuretsu! La la la!
 	 * @param player the {@link PlayerEntity} that activated the effect.
 	 * @param effect the {@link NbtCompound} containing information about the effect. The power param is used to determine explosive strength.
 	 * @param world the {@link World} the effect takes place in.
@@ -60,20 +76,23 @@ public final class ItemEffects {
 			world.createExplosion(player, player.getX(), player.getY(), player.getZ(), 10, Explosion.DestructionType.DESTROY);
 		}
 	}
+
 	/**
-	 * The Lightning method. Smites an area depending on the power parameter.
+	 * Your life is NOTHING. You serve ZERO purpose.
 	 * @param player the {@link PlayerEntity} that activated the effect.
 	 * @param effect the {@link NbtCompound} containing information about the effect.
 	 *               If the power parameter is a {@link NbtString} with a value of {@code NOW} then smite randomly around the player in a 16x8x16 cube.
 	 *               If the power parameter is an {@link NbtInt} then smite the {@code player.raycast()} location {@code power} times. The range is also determined by {@code power}.
 	 * @param world the {@link World} the effect takes place in.
 	 **/
-	private void YouShouldKillYourselfNOW(PlayerEntity player, NbtCompound effect, World world){
+	private void YouShouldKillYourselfNOW(ServerPlayerEntity player, NbtCompound effect, World world){
 
 		if (effect.get("power") instanceof NbtString power && power.asString().equals("NOW")){
 				int iterations = (int) (Math.round(Math.random() * 5) + 5);
 				for (int i = 0; i < iterations; i++) {
 					LightningEntity boomBooms = new LightningEntity(EntityType.LIGHTNING_BOLT, world);
+					boomBooms.setChanneler(player);
+					boomBooms.damage(DamageSource.player(player), 20);
 					float offsetX = (Math.round(Math.random() * 8));
 					float offsetY = (Math.round(Math.random() * 8) - 4);
 					float offsetZ;
@@ -89,6 +108,8 @@ public final class ItemEffects {
 			Vec3d location = player.raycast(power.intValue()*5, 2f, true).getPos();
 			for (int i = 0; i < power.intValue(); i++){
 				LightningEntity boomBooms = new LightningEntity(EntityType.LIGHTNING_BOLT, world);
+				boomBooms.setChanneler(player);
+				boomBooms.damage(DamageSource.player(player), 20);
 				boomBooms.setPos(location.x,location.y,location.z);
 				world.spawnEntity(boomBooms);
 			}
